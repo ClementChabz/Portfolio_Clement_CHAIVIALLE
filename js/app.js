63263f3a3458d180484dd3b37d58c156d038fc5f
Chaivialle-Bazin Clement <clement.chaivialle@student-cs.fr>
2025-03-16 01:04:01 +0100

    Page main épurée

--- js/app.js
+++ js/app.js
@@ -8,13 +8,27 @@ menu.addEventListener('click',function(){
 });
 
 
-document.querySelectorAll(".footer__link--items h2").forEach((heading) => {
-    const text = heading.textContent;
-    heading.innerHTML = text
+document.querySelectorAll(".footer__link--items h2 a").forEach((link) => {
+    const text = link.textContent;
+    link.innerHTML = text
         .split("")
-        .map((char, index) => 
+        .map((char, index) =>
             `<span style="animation-delay: ${index * 0.1}s">${char}</span>`
         )
         .join("");
 });
 
+let card1 = document.getElementById("carte1");
+card1.addEventListener("click", () => {
+    window.location.href = './html/resume.html';
+});
+
+let card2 = document.getElementById("carte2");
+card2.addEventListener("click", () => {
+    window.location.href = './html/etudiant.html';
+});
+
+let card3 = document.getElementById("carte3");
+card3.addEventListener("click", () => {
+    window.location.href = './html/projects.html';
+});
